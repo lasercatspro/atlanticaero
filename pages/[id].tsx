806857680c87e128703d46383b91219f54dfc8be
FraBr <brault.fra@gmail.com>
2021-05-31 15:54:29 +0200

    :bug: fixed consoel.log

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,5 +1,21 @@
 import React from 'react'
 
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { ... } } // See the "paths" section below
+    ],
+    fallback: true or false // See the "fallback" section below
+  };
+}
+
+export async function getStaticProps() {
+  const document = await Client.getSingle("landing", {})
+  return {
+    props: {document},
+  }
+}
+
 export default function Article() {
   return (
     <div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export async function getStaticProps() {
 
 export default function Home(props:any) {
   useEffect(() => {
-    console.log(props.data)
+    console.log(props.document)
   }, [])
   return (
     <Layout>
