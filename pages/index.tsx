dbbc7f36cc9d6bd2903ca6cee8e92ce65ef2e06a
FraBr <brault.fra@gmail.com>
2021-05-31 15:45:27 +0200

    :sparkles: prismic linked to app and working like a charm

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,20 @@
+import {Client} from '../prismic-config'
 import Head from 'next/head'
 import { Companies, Features, Hero, Who, Plane, Testimonials, Footer, Layout } from '../components';
+import { useEffect } from 'react';
+
+export async function getStaticProps() {
+  const document = await Client.getSingle("landing", {})
+  return {
+    props: {document},
+  }
+}
 
 
-export default function Home() {
+export default function Home(props:any) {
+  useEffect(() => {
+    console.log(props.data)
+  }, [])
   return (
     <Layout>
       <Hero />
--- prismic-config.ts
+++ prismic-config.ts
@@ -1,7 +1,6 @@
-const Prismic = require('@prismicio/client');
+import Prismic from '@prismicio/client';
 
 // Update your-repo-name with the name of your repository.
 const apiEndpoint = 'https://atlanticaero.prismic.io/api/v2'
-const Client = Prismic.client(apiEndpoint, {  })
+export const Client = Prismic.client(apiEndpoint)
 
-export default Client
