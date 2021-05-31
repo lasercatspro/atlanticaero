13b00b8d8bd384a0e08007ad3d6105c080f1acad
Werner F <wernerfinck@netcourrier.com>
2021-05-31 12:50:57 +0200

    :tada: INIT: all landning section with tailwindUI components

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,12 @@
-import Home from './Hero';
+import Hero from './Hero';
+import Companies from './Companies';
+import Features from './Features';
+import Who from './Who';
+import Plane from './Plane';
+import Testimonials from './Testimonials';
+import Footer from './Footer';
+import NavBar from './NavBar';
+import QuoteIcon from './QuoteIcon';
+import Layout from './Layout';
 
-export { Home }
+export { Hero, Companies, Features, Who, Plane, Testimonials, Footer, NavBar, QuoteIcon, Layout }
