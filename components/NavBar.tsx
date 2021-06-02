253b0917bcd65d6a8a8ef7fc3818dfbb29d4c3cd
Werner F <wernerfinck@netcourrier.com>
2021-06-03 01:33:45 +0200

    :construction: WIP: brand name

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -105,24 +105,25 @@ const NavBar = () => {
       {({ open }) => (
         <>
           <div className="px-4 mx-auto max-w-7xl sm:px-6">
-            <div className="flex items-center justify-between py-3 md:justify-start md:space-x-10">
+            <div className="flex items-center justify-between py-3 lg:justify-start lg:space-x-10">
               <div className="flex justify-start lg:w-0 lg:flex-1">
-                <a href="#">
-                  <span className="sr-only">Workflow</span>
+                <a className="text-xl font-black uppercase" href="/">
+                  Bocage AirLines
+                  {/* <span className="sr-only">Workflow</span>
                   <img
                     className="w-auto h-8 sm:h-10"
                     src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
                     alt=""
-                  />
+                  /> */}
                 </a>
               </div>
-              <div className="-my-2 -mr-2 md:hidden">
+              <div className="-my-2 -mr-2 lg:hidden">
                 <Popover.Button className="inline-flex items-center justify-center p-2 text-gray-400 bg-white rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                   <span className="sr-only">Open menu</span>
                   <MenuIcon className="w-6 h-6" aria-hidden="true" />
                 </Popover.Button>
               </div>
-              <div className="hidden space-x-4 md:flex">
+              <div className="hidden space-x-6 lg:flex">
                 {navigation.map(item => <a
                   href={item.href}
                   className="text-base font-medium text-gray-500 hover:text-gray-900"
@@ -131,7 +132,7 @@ const NavBar = () => {
                 </a>)}
               </div>
 
-              <div className="items-center justify-end hidden md:flex md:flex-1 lg:w-0">
+              <div className="items-center justify-end hidden lg:flex lg:flex-1 lg:w-0">
                 <ContactButton className="inline-flex items-center justify-center px-4 py-2 ml-8 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm whitespace-nowrap hover:bg-indigo-700">
                   Volez
                 </ContactButton>
@@ -152,17 +153,20 @@ const NavBar = () => {
             <Popover.Panel
               focus
               static
-              className="absolute inset-x-0 top-0 z-10 p-2 transition origin-top-right transform md:hidden"
+              className="absolute inset-x-0 top-0 z-10 p-2 transition origin-top-right transform lg:hidden"
             >
               <div className="bg-white divide-y-2 rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 divide-gray-50">
                 <div className="px-5 pt-5 pb-6">
                   <div className="flex items-center justify-between">
                     <div>
-                      <img
-                        className="w-auto h-8"
-                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                        alt="Workflow"
-                      />
+                      <a className="text-lg font-black uppercase" href="/">
+                        Bocage AirLines
+                        {/*<img
+                            className="w-auto h-8"
+                            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                            alt="Workflow"
+                          /> */}
+                      </a>
                     </div>
                     <div className="-mr-2">
                       <Popover.Button className="inline-flex items-center justify-center p-2 text-gray-400 bg-white rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
