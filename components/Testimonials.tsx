34ed3dab468d274aa3a8437ee3f0298a20891f76
Werner F <wernerfinck@netcourrier.com>
2021-06-03 12:45:56 +0200

    :lipstick: RESPONSIVE: on testimonials section 3

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -15,18 +15,18 @@ const Testimonials = ({ testimonials }: Props) => {
           <h2 className="text-3xl font-extrabold tracking-tight sm:text-4xl">
             {RichText.asText(testimonials.primary.title)}
           </h2>
-          <ul className="space-y-12 md:grid md:grid-cols-2 md:items-start md:gap-x-8 md:gap-y-12 md:space-y-0">
+          <ul className="space-y-12 lg:w-auto sm:grid sm:grid-cols-2 sm:items-start sm:gap-x-8 sm:gap-y-12 sm:space-y-0">
             {testimonials.items.map((testimonial, i) => (
               <li key={testimonial.name}>
-                <div className="flex space-x-4 md:space-y-4 h-36 md:h-auto md:grid md:grid-cols-3 md:gap-8 md:space-y-0">
+                <div className="flex space-x-4 lg:space-y-4 h-36 lg:h-auto lg:grid lg:grid-cols-3 lg:gap-8 lg:space-y-0">
                   {/* image */}
-                  <div className="w-24 h-24 md:w-auto md:h-0 aspect-w-3 aspect-h-2 sm:aspect-w-3 sm:aspect-h-4">
+                  <div className="w-24 h-24 lg:w-auto lg:h-0 aspect-w-3 aspect-h-2 sm:aspect-w-3 sm:aspect-h-4">
                     <Image
                       layout="responsive"
                       width={testimonial.image.dimensions.width}
                       height={testimonial.image.dimensions.height}
 
-                      className="object-contain rounded-lg shadow-lg md:object-cover"
+                      className="object-contain rounded-lg shadow-lg lg:object-cover"
                       src={testimonial.image.url}
                       alt={
                         testimonial.image.alt
@@ -41,11 +41,11 @@ const Testimonials = ({ testimonials }: Props) => {
                       <div className="space-y-1 text-lg font-medium leading-6">
                         <h3>{testimonial.name}</h3>
                         <QuoteIcon />
-                        <p className="hidden text-indigo-600 md:block">
+                        <p className="hidden text-indigo-600 lg:block">
                           {RichText.asText(testimonial["people-desc"])}
                         </p>
                       </div>
-                      <div className="hidden text-lg md:block">
+                      <div className="hidden text-lg lg:block">
                         <p className="text-gray-500">
                           {RichText.asText(testimonial.testimonial)}
                         </p>
@@ -56,8 +56,8 @@ const Testimonials = ({ testimonials }: Props) => {
 
                 </div>
                 {/* au dessous de lg on passe le texte sous la photo et le nom */}
-                <div className="block text-lg md:hidden">
-                  <p className="hidden text-indigo-600 md:block">
+                <div className="block text-lg lg:hidden">
+                  <p className="hidden text-indigo-600 lg:block">
                     {RichText.asText(testimonial["people-desc"])}
                   </p>
                   <p className="text-gray-500">
